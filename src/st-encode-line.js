790068f3751d9b46aac9ca9db184174657226a6d
nastassia-batashova <batashofa@gmail.com>
2021-09-17 20:59:30 +0300

    feat: added common character count solution

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -1,24 +1,16 @@
-// import { NotImplementedError } from '../extensions/index.js';
-//
-// /**
-//  * Given a string, return its encoding version.
-//  *
-//  * @param {String} str
-//  * @return {String}
-//  *
-//  * @example
-//  * For aabbbc should return 2a3bc
-//  *
-//  */
-// export default function encodeLine(/* str */) {
-//   throw new NotImplementedError('Not implemented');
-//   // remove line with error and write your code here
-// }
+import { NotImplementedError } from '../extensions/index.js';
 
-function encodeLine(str) {
-    let arr = str.split('');
-    let crypted = arr.map(value => value.charCodeAt(0) ^ 1).join('')
-    return crypted;
+/**
+ * Given a string, return its encoding version.
+ *
+ * @param {String} str
+ * @return {String}
+ *
+ * @example
+ * For aabbbc should return 2a3bc
+ *
+ */
+export default function encodeLine(/* str */) {
+  throw new NotImplementedError('Not implemented');
+  // remove line with error and write your code here
 }
-
-console.log(encodeLine('aaaatttt'))
